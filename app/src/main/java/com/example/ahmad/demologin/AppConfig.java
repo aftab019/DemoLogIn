1534c8f4c6024447ef244dfa67dfd320598425fb
aftab019 <fa13bce019@gmail.com>
2017-08-08 10:22:11 +0500

    Initial commit

---
+++ app/src/main/java/com/example/ahmad/demologin/AppConfig.java
@@ -0,0 +1,18 @@
+package com.example.ahmad.demologin;
+
+/**
+ * Created by Ahmad on 8/4/2017.
+ */
+
+public class AppConfig {
+
+
+        // Server user login url
+        public static String URL_LOGIN = "http://192.168.8.143/android_login_api/include/login.php";
+
+        // Server user register url
+        public static String URL_REGISTER = "http://192.168.8.143/android_login_api/include/register.php";
+
+
+
+}
